beeb77150de2d53b410323af1b8eeaf12a42fd38
chj0103 <>
2024-12-08 14:57:35 +0900

    add carousel

---
+++ js/prpductCarousel.js
@@ -0,0 +1,14 @@
+const slider = tns({
+	container: '.product-carousel .slider-list',
+	items: 1,
+	controls: false,
+	nav: true,
+	  navContainer: '.product-carousel .thumbnail-list',
+	navAsContainer: true,
+	arrowKeys: true,
+	autoplay: false,
+	autoplayHoverPause: true,
+	autoplayButtonOutput: false,
+	mouseDrag: true,
+	preventScrollOnTouch: true
+});
